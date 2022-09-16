3e9580db8fe79dc3e27ad4acdac496059ccef32b
FernsonJ <112515169+FernsonJ@users.noreply.github.com>
2022-09-16 09:58:09 -0400

    Create PlayerData.js

---
+++ js/data/PlayerData.js
@@ -0,0 +1,49 @@
+export default {
+  movement: {
+    controls: {
+      forward: ["KeyW", "ArrowUp"],
+      backward: ["KeyS", "ArrowDown"],
+      left: ["KeyA", "ArrowLeft"],
+      right: ["KeyD", "ArrowRight"],
+
+      jump: ["Space"],
+      crouch: ["ShiftLeft"],
+
+      //hotbar 1-9
+      hotbar: [
+        ["Digit1"],
+        ["Digit2"],
+        ["Digit3"],
+        ["Digit4"],
+        ["Digit5"],
+        ["Digit6"],
+        ["Digit7"],
+        ["Digit8"],
+        ["Digit9"],
+      ],
+    },
+
+    horizontalSpeed: 800,
+    verticalSpeed: 600,
+    lookSpeed: 800,
+
+    defaultSpawnLocation: [6, 16, 6],
+  },
+
+  blockInteraction: {
+    placementDistance: 3,
+    breakDistance: 3,
+  },
+
+  defaultHotbar: [
+    "grass",
+    "dirt",
+    "stone",
+    "cobblestone",
+    "stoneBricks",
+    "oakPlanks",
+    "oakWood",
+    "bedrock",
+    "",
+  ],
+};
