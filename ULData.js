56b570ba236d143b0154b5611749b1d800442d4f
FernsonJ <112515169+FernsonJ@users.noreply.github.com>
2022-09-16 10:00:12 -0400

    Create ULData.js

---
+++ ULData.js
@@ -0,0 +1,38 @@
+export default {
+  /*
+  [
+    left
+    top
+    width
+    height
+  ]
+  */
+  hotbarScale: 1,
+  hotbar: [
+    304,
+    525,
+    392,
+    44,
+  ],
+  hotbarSelected: [
+    302,
+    525,
+    50,
+    50,
+  ],
+  icons: {
+    size: 28,
+  },
+  //x offset added per slot index
+  selectorIndexOffset: -0.9,
+  selectorXOffset: 0.6,
+  selectorYOffset: -3,
+
+  //x offset added per slot index
+  iconIndexOffset: -0.9,
+  iconXOffset: 1.8,
+  iconYOffset: 0.3,
+
+  //opacity 0-1
+  hotbarItemOpacity: 1,
+};
